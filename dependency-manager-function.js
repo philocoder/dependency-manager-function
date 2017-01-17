78609ffc6afe3e05a50d875070ef92a05bb828fc
philocoder <carlosjimenohernandez@gmail.com>
2017-01-17 13:43:23 +0100

    Add files via upload

---
+++ dependency-manager-function.js
@@ -0,0 +1,32 @@
+function implementDefineRequirePattern( baseObject ) {
+  var base = baseObject;
+  base.defined = {};
+  base.required = [];
+  base.define = function(name, dependencies, mod) {
+    if(!(typeof name === "string")) 
+      throw new TypeError("Module's define method | Parameter 1 must be a string");
+    if(!(dependencies instanceof Array)) 
+      throw new TypeError("Module's define method | Parameter 1 must be a string");
+    if(!(name in base.defined)) {
+      base.defined[name] = {
+        name: name,
+        dependencies: dependencies,
+        module: mod
+      };
+    } else {};
+  };
+  base.require = function(name) {
+    if(!(name in base.defined)) {
+      throw new Error("Module " + name + " included but not defined.");
+    }
+    if(base.required.indexOf(name) === -1) {
+      var mod = base.defined[name];
+      for(var a=0; a<mod.dependencies.length; a++) {
+        base.require(mod.dependencies[a]);
+      }
+      base.required.push(name);
+    }
+    return base.defined[name].module;
+  };
+  return base;
+};
